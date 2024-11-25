88cb5ab03c9f6e97d88eb87fa5b08e8742aa5852
Ana Paula Martins Ribeiro <ana.martins@cieemg.bhz>
2024-11-25 14:29:48 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='section-home'>
+        <h1>oi</h1>
+        <h1>oi</h1>
+        <h1>oi</h1>
+        <h1>oi</h1>
+        <h1>oi</h1>
+        <h1>oi</h1>
+        <h1>oi</h1>
+        <h1>oi</h1>
+        <h1>oi</h1>
+        <h1>oi</h1>
+        <h1>oi</h1>
+        <h1>oi</h1>
+        <h1>oi</h1>
+        <h1>oi</h1>
+        <h1>oi</h1>
+        <h1>oi</h1>
+        <h1>oi</h1>
+        <h1>oi</h1>
+        <h1>oi</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
